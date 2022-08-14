8d003b82b1aa082005d816ad4a6b79a60106a0a0
Maria Pavlenko <111146792+MlPavlenko@users.noreply.github.com>
2022-08-14 15:32:04 +0300

    C# course

---
+++ firstproject/firstproject/Program.cs
@@ -0,0 +1,18 @@
+﻿string clientFirstName;
+string clientLastName;
+string clientMiddleName;
+
+Console.WriteLine("Как Вас зовут?");
+clientFirstName = Console.ReadLine();
+Console.WriteLine("Привет, "+ clientFirstName + "!!!");
+
+Console.WriteLine("Какая у Вас фамилия?");
+clientLastName = Console.ReadLine();
+
+Console.WriteLine("Какое у Вас отчество?");
+clientMiddleName = Console.ReadLine();
+
+string clientAllName=clientFirstName+" "+clientLastName+" "+clientMiddleName;
+Console.WriteLine(clientAllName);
+
+
