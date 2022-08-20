41f2e795d97cd99aef6bf6b97fc37f2db401d0db
Maria Pavlenko <111146792+MlPavlenko@users.noreply.github.com>
2022-08-20 18:19:09 +0300

    lesson 06\30

---
+++ secondproject/secondproject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 11;
+            int b = 100;
+
+            int c = a * b;
+            Console.WriteLine(c);
+
+            var d = (double)b / a;
+            Console.WriteLine(d);
+
+            Console.ReadLine();
+        }
+    }
+}
